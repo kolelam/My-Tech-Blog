cd30abc0ab67e8acbc70fc3df8f4f338736443ee
Kole Lam <kolelamied1@gmail.com>
2023-07-23 17:15:49 -0600

    added comments and bug fixes

--- models/index.js
+++ models/index.js
@@ -1,9 +1,24 @@
-const router = require('express').Router();
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
 
-const user = require('./user');
-const post = require('./post');
+// Define relationships
+User.hasMany(Post, {
+    foreignKey: 'author_id'
+});
 
-router.use('/user', user);
-router.use('/post', post);
+Post.belongsTo(User, {
+    foreignKey: 'author_id'
+});
 
-module.exports = router;
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+
+
+module.exports = { User, Post, Comment }
